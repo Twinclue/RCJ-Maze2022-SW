5b33ddb558f60ccac2b605075a4c41ad8f1dc7a0
K_HazK <hajime.nishimura0502@gmail.com>
2021-10-27 02:01:00 +0900

    vl6180 テスト

--- main-teensy/src/main.cpp
+++ main-teensy/src/main.cpp
@@ -1,23 +1,39 @@
 #include <Arduino.h>
+#include <PCA9547.h>
+#include <VL53L0X.h>
+#include <VL6180X.h>
 #include "read_imu.h"
 
 read_imu sensor;
+PCA9547 i2cSelect;
+VL6180X vl61a;
+
+const int port = 0;
 
 void setup()
 {
   Serial.begin(115200);
+  i2cSelect.attatch(Wire2);
   Wire2.begin();
-
+  vl61a.setBus(&Wire2);
+  i2cSelect.enable(port);
+  vl61a.init();
+  vl61a.configureDefault();
+  vl61a.setScaling(1);
+  vl61a.setTimeout(500);
+  i2cSelect.disable();
+  
   delay(100);
-  sensor.begin(&Wire2);
 }
 
 void loop()
 {
-  sensor.read();
-  float yaw   = sensor.getYaw();
-
-  Serial.print(yaw, 3);
-  Serial.println();
+  i2cSelect.enable(port);
+  int dist = vl61a.readRangeSingleMillimeters();
+  if (vl61a.timeoutOccurred()){
+      Serial.println("failed!");
+  }
+  i2cSelect.disable();
+  Serial.println(dist);
   delay(100);
 }
