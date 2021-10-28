e2ae43283835bffcbe62d7fe0825ba3ba13e9bc3
K_HazK <hajime.nishimura0502@gmail.com>
2021-10-28 23:50:29 +0900

    read関数追加

--- main-teensy/lib/read_tof/read_tof.h
+++ main-teensy/lib/read_tof/read_tof.h
@@ -8,7 +8,7 @@
 class read_tof{
     public:
         read_tof(TwoWire *_bus);
-
+        int read(uint8_t _direction);
     private:
         const int vl61Num = 4;
         const int vl53Num = 1;
@@ -16,4 +16,5 @@ class read_tof{
         VL6180X vl61[VL61NUM];
         VL53L0X vl53[VL53NUM];
         TwoWire *bus;
+        int dist;
 };
--- main-teensy/src/main.cpp
+++ main-teensy/src/main.cpp
@@ -11,34 +11,18 @@ const int port = 0;
 
 void setup()
 {
-/*
-  Serial.begin(115200);
-  i2cSelect.attatch(Wire2);
-  Wire2.begin();
-
-  vl61a.setBus(&Wire2);
-  i2cSelect.enable(port);
-  vl61a.init();
-  vl61a.configureDefault();
-  vl61a.setScaling(1);
-  vl61a.setTimeout(500);
-  i2cSelect.disable();
-*/
-
   delay(100);
 }
 
 void loop()
 {
-/*
-  i2cSelect.enable(port);
-  int dist = vl61a.readRangeSingleMillimeters();
-  if (vl61a.timeoutOccurred()){
-      Serial.println("failed!");
+  
+  for(int i=0;i<5;i++){
+    Serial.print(tof.read(i));
+    Serial.print(" : ");
   }
-  i2cSelect.disable();
-
-  Serial.println(dist);
-*/
+  
+  //Serial.print(tof.read(4));
+  Serial.println();
   delay(100);
 }
