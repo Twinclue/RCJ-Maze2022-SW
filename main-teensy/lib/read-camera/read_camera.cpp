993e535434850cc0c4ef0286ea158ce06eaa55ba
kossansato <kossan.sato@gmail.com>
2021-11-11 02:09:12 +0900

    make read_camera.cpp

--- main-teensy/lib/read-camera/read_camera.cpp
+++ main-teensy/lib/read-camera/read_camera.cpp
@@ -1,2 +1,18 @@
 #include "read_camera.h"
 
+read_camera::read_camera(uint8_t _txPin, uint8_t _rxPin){
+	txPin=_txPin;
+	rxPin=_rxPin;
+	pinMode(txPin, OUTPUT);
+	pinMode(rxPin, OUTPUT);
+}
+
+int read_camera::read(){
+	bool txIn, rxIn;
+	txIn=digitalRead(txPin);
+	rxIn=digitalRead(rxPin);
+	if(txIn==0&&rxIn==0){return 0;} //no victim
+	if(txIn==1&&rxIn==0){return 1;} //alert
+	if(txIn==0&&rxIn==1){return 2;} //1 kit
+	if(txIn==1&&rxIn==1){return 3;} //2 kits
+}
--- main-teensy/lib/read-camera/read_camera.h
+++ main-teensy/lib/read-camera/read_camera.h
@@ -8,8 +8,8 @@ class read_camera{
 		read_camera(uint8_t _txPin, uint8_t _rxPin);
 		int read();
 	private:
-		uint8_t txPin
-		uint8_t rxPin
+		uint8_t txPin //Comm2
+		uint8_t rxPin //Comm1
 };
 
 #endif
