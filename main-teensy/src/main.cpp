c69e4b7a3f8628355b3991d13978b9cbb93bb3ca
K_HazK <hajime.nishimura0502@gmail.com>
2021-11-21 13:00:43 +0900

    モーターテスト追加

--- main-teensy/src/main.cpp
+++ main-teensy/src/main.cpp
@@ -2,11 +2,19 @@
 #include "read_imu.h"
 #include "read_tof.h"
 #include "advanced_tof.h"
+#include "drive_motor.h"
 
 read_imu imu;
 read_tof toff(&Wire2);
 read_tof tofb(&Wire);
 
+drive_motor left(35,36,37,15,16);
+drive_motor right(38,39,14,15,16);
+
+
+
+char buff[30];
+int i=0;
 void setup()
 {
   Serial.begin(19200);
@@ -18,6 +26,20 @@ void setup()
 
 void loop()
 {
+
+  if (Serial.available() > 0) {
+    buff[i] = Serial.read();
+    if (buff[i] == '\n') {
+      buff[i] = '\0';
+      right.on(atoi(strtok(buff, ":")));
+      left.on(atoi(strtok(NULL, ":")));
+      i = 0;
+    } else {
+      i += 1;
+    }
+  }
+
+
   imu.read();
   for(int i=0;i<5;i++){// fls : flf : frf : frs : fc
     Serial.print(toff.read(i));
@@ -40,3 +62,4 @@ void loop()
 
   delay(50);
 }
+
