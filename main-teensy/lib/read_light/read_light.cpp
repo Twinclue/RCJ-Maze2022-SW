68176b230b14b610c47cd82ea8c4bfe6c5236c62
K_HazK <hajime.nishimura0502@gmail.com>
2021-12-07 16:13:16 +0900

    read_light実装

--- main-teensy/lib/read_light/read_light.cpp
+++ main-teensy/lib/read_light/read_light.cpp
@@ -2,4 +2,25 @@
 
 read_light::read_light(Adafruit_NeoPixel *_led){
     led = _led;
+    pinMode(emitter,INPUT);
+    led->begin();
+    led->setPixelColor(lednum,cr,cg,cb);
+    led->setBrightness(255);
+    led->show();
+}
+
+int read_light::getFloorColor(){
+    if(this->read() < thresholdBW){
+        return 1;
+    }
+    else if(this->read() < thresholdWS){
+        return 0;
+    }
+    else{
+        return 2;
+    }
+}
+
+int read_light::read(){
+    return analogRead(emitter);
 }
--- main-teensy/src/main.cpp
+++ main-teensy/src/main.cpp
@@ -4,26 +4,18 @@
 #include "read_tof.h"
 #include "advanced_tof.h"
 #include "drive_motor.h"
+#include "read_light.h"
 
 Adafruit_NeoPixel npix = Adafruit_NeoPixel(2,26, NEO_GRB + NEO_KHZ800);
+read_light light(&npix);
 
 void setup()
 {
-  pinMode(22,INPUT);
-  npix.begin();
-  npix.setPixelColor(1,255,0,0);
-  npix.setBrightness(255);
-  npix.show();
-  delay(100);
+
 }
 
 void loop()
 {
-  Serial.println(analogRead(22));
-  npix.setPixelColor(0,255,255,255);
-  npix.show();
-  delay(500);
-  npix.setPixelColor(0,0,0,0);
-  npix.show();
-  delay(500);
+  Serial.println(light.read());
+  delay(100);
 }
