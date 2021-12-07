557273d086b06461fe51af8b70c1fb403656459b
kossansato <kossan.sato@gmail.com>
2021-12-08 02:42:24 +0900

    paste D6Tarduino library

---
+++ main-teensy/lib/read_temperature/lib/read_temperature.h
@@ -0,0 +1,13 @@
+#ifndef READ_TEMPERATURE_H_
+#define READ_TEMPERATURE_H_
+
+#include <Arduino.h>
+#include "D6Tarduino.h"
+
+class read_temperature{
+    public:
+    read_temperature();
+    private:
+
+};
+#endif
