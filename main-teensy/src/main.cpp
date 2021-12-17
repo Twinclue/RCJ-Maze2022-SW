cf44483adb1d99c0388e70c2b9804d700f053a16
K_HazK <hajime.nishimura0502@gmail.com>
2021-12-17 23:27:47 +0900

    送信する値追加

--- main-teensy/src/main.cpp
+++ main-teensy/src/main.cpp
@@ -1,5 +1,5 @@
 #include <Arduino.h>
-#include "read_imu.h"
+#include "move_robot.h"
 #include "read_tof.h"
 #include "advanced_tof.h"
 #include "drive_motor.h"
@@ -8,10 +8,17 @@ read_imu imu;
 read_tof toff(&Wire2);
 read_tof tofb(&Wire);
 
-drive_motor left(35,36,37,15,16);
-drive_motor right(38,39,14,15,16);
+drive_motor leftM(35,36,37,15,16);
+drive_motor rightM(38,39,14,15,16);
 
+Adafruit_NeoPixel npix = Adafruit_NeoPixel(2,26, NEO_GRB + NEO_KHZ800);
 
+read_light light(&npix);
+
+move_robot move(&leftM,&rightM,&toff,&tofb,&imu,&light);
+
+const byte lTouch = 21;
+const byte rTouch = 17;
 
 char buff[30];
 int i=0;
@@ -21,6 +28,8 @@ void setup()
   Wire2.begin();
   Wire.begin();
   imu.begin(&Wire2);
+  pinMode(rTouch,INPUT);
+  pinMode(lTouch,INPUT);
   delay(100);
 }
 
@@ -31,8 +40,14 @@ void loop()
     buff[i] = Serial.read();
     if (buff[i] == '\n') {
       buff[i] = '\0';
-      right.on(atoi(strtok(buff, ":")));
-      left.on(atoi(strtok(NULL, ":")));
+      rightM.on(atoi(strtok(buff, ":")));
+      leftM.on(atoi(strtok(buff, ":")));
+      if(atoi(strtok(buff,":"))){
+        move.drop(true);
+      }
+      else if(atoi(strtok(NULL,":"))){
+        move.drop(false);
+      }
       i = 0;
     } else {
       i += 1;
@@ -56,6 +71,14 @@ void loop()
   Serial.print(imu.getPitch());
   Serial.print(":");
   Serial.print(imu.getYaw());
+  Serial.print(":");
+  Serial.print(light.read());
+  Serial.print(":");
+  Serial.print(light.getFloorColor());
+  Serial.print(":");
+  Serial.print(digitalRead(rTouch));
+  Serial.print(":");
+  Serial.print(digitalRead(lTouch));
   Serial.print(":");
 
   Serial.println();
