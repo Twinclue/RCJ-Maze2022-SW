578308d6c943aefd86f059953a838de15ed02937
kossansato <kossan.sato@gmail.com>
2022-03-05 14:32:27 +0900

    modify for current sensor test
    make sure to set clock freq of ATtiny85 to 1MHz

--- main-ATtiny/BatteryCheck_ArduinoIDE/BatteryCheck_ArduinoIDE.ino
+++ main-ATtiny/BatteryCheck_ArduinoIDE/BatteryCheck_ArduinoIDE.ino
@@ -1,50 +1,65 @@
 #include <Adafruit_INA219.h>
 #include <SoftwareSerial.h>
 #define trigPin 3
-#define main_loop_delay 1000
+#define VIOUTpin 4
+#define main_loop_delay 10
 
-// SoftwareSerial mySerial(1,4); // RX, TX
-
+SoftwareSerial mySerial(2,0); // RX, TX
 Adafruit_INA219 ina219(0X41);
+
 float voltage=0.0;
 const float minCV=10.6;
+
+const int VIOUT_offset=0;
+const float VIOUT_coef=1.0;
+float current=0.0;
+
 bool Trig=false;
 bool prevTrig=false;
 bool cutoff=false;
 
+float getCurrent(){
+	current=(analogRead(VIOUTpin)+VIOUT_offset)*VIOUT_coef;
+	return current;
+}
+
 void setup(){
 	pinMode(trigPin,OUTPUT);
-	digitalWrite(trigPin, LOW);
-	// mySerial.begin(4800);
+	digitalWrite(trigPin, HIGH);
+	mySerial.begin(4800);
 	// while(!mySerial.available()){
 	// 	delay(10);
 	// }
-	while(!ina219.begin()){
-		// mySerial.println("INA_fail");
-	}
-	ina219.setCalibration_16V_400mA();
+	// while(!ina219.begin()){
+	// 	// mySerial.println("INA_fail");
+	// }
+	// ina219.setCalibration_16V_400mA();
 }
 
 void loop(){
-	while(!ina219.success()){
-		digitalWrite(trigPin, false);
-	}
-	voltage=ina219.getBusVoltage_V();
-	// mySerial.println(voltage);
-	// mySerial.println(ina219.success());
+	// while(!ina219.success()){
+	// 	digitalWrite(trigPin, false);
+	// }
+	// voltage=ina219.getBusVoltage_V();
 
-	if(prevTrig==true){		//last time voltage was high enough 
-		Trig=voltage>minCV;
-	}else{
-		if(voltage>minCV){	//voltage recovered
-			Trig=true;
-		}else{				//voltage is lower than Vth for 1 second
-			Trig=false;
-			cutoff=true;
-			}
-	}
-	digitalWrite(trigPin, !cutoff);
-	while(cutoff);
-	prevTrig=Trig;
+	// // mySerial.println(voltage);
+	// // mySerial.println(ina219.success());
+
+	// if(prevTrig==true){		//last time voltage was high enough
+	// 	Trig=voltage>minCV;
+	// }else{
+	// 	if(voltage>minCV){	//voltage recovered
+	// 		Trig=true;
+	// 	}else{				//voltage is lower than Vth for 1 second
+	// 		Trig=false;
+	// 		cutoff=true;
+	// 		}
+	// }
+	// digitalWrite(trigPin, !cutoff);
+	// while(cutoff);
+	// prevTrig=Trig;
+
+	float current=getCurrent();
+	mySerial.println(current);
 	delay(main_loop_delay);
 }
