0e25c3c3b50eacb440ef9117ef9b2524c979a3f2
kossansato <kossan.sato@gmail.com>
2022-03-07 02:01:57 +0900

    make getCurrent func with LPF

--- main-ATtiny/BatteryCheck_ArduinoIDE/BatteryCheck_ArduinoIDE.ino
+++ main-ATtiny/BatteryCheck_ArduinoIDE/BatteryCheck_ArduinoIDE.ino
@@ -1,8 +1,12 @@
 #include <Adafruit_INA219.h>
 #include <SoftwareSerial.h>
+
 #define trigPin 3
-#define VIOUTpin 4
+#define VIOUTpin A2
 #define main_loop_delay 10
+#define calibSampleNum 20
+#define BAT_resistance 0.0
+#define idleCurrent_mA 31
 
 SoftwareSerial mySerial(2,0); // RX, TX
 Adafruit_INA219 ina219(0X41);
@@ -10,30 +14,56 @@ Adafruit_INA219 ina219(0X41);
 float voltage=0.0;
 const float minCV=10.6;
 
-const int VIOUT_offset=0;
-const float VIOUT_coef=1.0;
+int VIOUT_offset=0;
+const float VIOUT_coef=0.0244;
 float current=0.0;
+
+const float LPFcoef=0.1;
+	float prevLPF;
+
 
 bool Trig=false;
 bool prevTrig=false;
 bool cutoff=false;
 
+int currentOffset(){
+	int sample=0;	//should be long enough for adding up all samples
+	for(uint8_t i=0; i<calibSampleNum; i++){
+		sample+=analogRead(VIOUTpin);
+		mySerial.println(sample);
+		delay(main_loop_delay);
+	}
+	return -sample/calibSampleNum;
+}
+
 float getCurrent(){
-	current=(analogRead(VIOUTpin)+VIOUT_offset)*VIOUT_coef;
-	return current;
+	float data=(analogRead(VIOUTpin)+VIOUT_offset)*VIOUT_coef;
+	return data;
+}
+
+float LPF(float input){
+	float LPF=LPFcoef*input+(1.0-LPFcoef)*prevLPF;
+	prevLPF=LPF;
+	return LPF;
 }
 
 void setup(){
+
 	pinMode(trigPin,OUTPUT);
-	digitalWrite(trigPin, HIGH);
+	digitalWrite(trigPin, LOW);
 	mySerial.begin(4800);
+
+	VIOUT_offset=currentOffset();
 	// while(!mySerial.available()){
 	// 	delay(10);
 	// }
+
 	// while(!ina219.begin()){
 	// 	// mySerial.println("INA_fail");
 	// }
 	// ina219.setCalibration_16V_400mA();
+
+	digitalWrite(trigPin, HIGH);	//delete when tests are finished
 }
 
 void loop(){
@@ -59,7 +89,11 @@ void loop(){
 	// while(cutoff);
 	// prevTrig=Trig;
 
-	float current=getCurrent();
-	mySerial.println(current);
+	current=LPF(getCurrent());
+	//VIOUT_offset=currentOffset();
+	//mySerial.print(analogRead(VIOUTpin));	mySerial.print("\t");
+	//mySerial.print(VIOUT_offset);	mySerial.print("\t");
+	mySerial.println(current+0.001*idleCurrent_mA);
+	mySerial.print("\n");
 	delay(main_loop_delay);
 }
