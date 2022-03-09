fc0d4a0c5f8bcd9e53002d1eca614b8f48a5d9d9
kossansato <kossan.sato@gmail.com>
2022-03-09 19:21:39 +0900

    increase BAT_resistance value
    It doesn't execute cutoff when a battery is charged and much current is run

--- main-ATtiny/BatteryCheck_ArduinoIDE/BatteryCheck_ArduinoIDE.ino
+++ main-ATtiny/BatteryCheck_ArduinoIDE/BatteryCheck_ArduinoIDE.ino
@@ -3,9 +3,9 @@
 
 #define trigPin 3
 #define VIOUTpin A2
-#define main_loop_delay 10
+#define main_loop_delay 40
 #define calibSampleNum 20
-#define BAT_resistance 0.0
+#define BAT_resistance 0.35	// according to the experiment data
 #define idleCurrent_mA 31
 
 SoftwareSerial mySerial(2,0); // RX, TX
@@ -21,7 +21,6 @@ float current=0.0;
 const float LPFcoef=0.1;
 	float prevLPF;
 
-
 bool Trig=false;
 bool prevTrig=false;
 bool cutoff=false;
@@ -30,8 +29,7 @@ int currentOffset(){
 	int sample=0;	//should be long enough for adding up all samples
 	for(uint8_t i=0; i<calibSampleNum; i++){
 		sample+=analogRead(VIOUTpin);
-		mySerial.println(sample);
-		delay(main_loop_delay);
+		delay(100);
 	}
 	return -sample/calibSampleNum;
 }
@@ -50,50 +48,49 @@ float LPF(float input){
 void setup(){
 
 	pinMode(trigPin,OUTPUT);
+	// pinMode(1, OUTPUT);
 	digitalWrite(trigPin, LOW);
-	mySerial.begin(4800);
+	// mySerial.begin(4800);
 
 	VIOUT_offset=currentOffset();
 	// while(!mySerial.available()){
 	// 	delay(10);
 	// }
 
-	// while(!ina219.begin()){
-	// 	// mySerial.println("INA_fail");
-	// }
-	// ina219.setCalibration_16V_400mA();
-
-	digitalWrite(trigPin, HIGH);	//delete when tests are finished
+	while(!ina219.begin()){
+		// mySerial.println("INA_fail");
+	}
+	ina219.setCalibration_16V_400mA();
 }
 
 void loop(){
-	// while(!ina219.success()){
-	// 	digitalWrite(trigPin, false);
-	// }
-	// voltage=ina219.getBusVoltage_V();
+	while(!ina219.success()){
+		digitalWrite(trigPin, LOW);
+
+	}
+	current=LPF(getCurrent())+0.001*idleCurrent_mA;
+	voltage=ina219.getBusVoltage_V()+current*BAT_resistance;	//copare with and without current calc
 
 	// // mySerial.println(voltage);
 	// // mySerial.println(ina219.success());
 
-	// if(prevTrig==true){		//last time voltage was high enough
-	// 	Trig=voltage>minCV;
-	// }else{
-	// 	if(voltage>minCV){	//voltage recovered
-	// 		Trig=true;
-	// 	}else{				//voltage is lower than Vth for 1 second
-	// 		Trig=false;
-	// 		cutoff=true;
-	// 		}
-	// }
-	// digitalWrite(trigPin, !cutoff);
-	// while(cutoff);
-	// prevTrig=Trig;
+	if(prevTrig==true){		//last time voltage was high enough
+		Trig=voltage>minCV;
+	}else{
+		if(voltage>minCV){	//voltage recovered
+			Trig=true;
+			cutoff=false;
+		}else{				//voltage is lower than Vth for 1 second
+			Trig=false;
+			cutoff=true;
+			}
+	}
+	digitalWrite(trigPin, !cutoff);
+	while(cutoff);	//prolong cutoff state until manual shutdown
+	prevTrig=Trig;
 
-	current=LPF(getCurrent());
-	//VIOUT_offset=currentOffset();
-	//mySerial.print(analogRead(VIOUTpin));	mySerial.print("\t");
-	//mySerial.print(VIOUT_offset);	mySerial.print("\t");
-	mySerial.println(current+0.001*idleCurrent_mA);
-	mySerial.print("\n");
+	// digitalWrite(trigPin, HIGH);
+	// mySerial.println(current);
+
 	delay(main_loop_delay);
 }
