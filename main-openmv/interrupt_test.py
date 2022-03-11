329e29a3bf2773fe30c7130fd25ac409d0d351b3
K_HazK <hajime.nishimura0502@gmail.com>
2022-03-11 17:13:26 +0900

    状態と割り込み設定ボタンの処理追加

--- main-openmv/interrupt_test.py
+++ main-openmv/interrupt_test.py
@@ -1,11 +1,13 @@
 # Untitled - By: hajim - 土 1 15 2022
 
 import sensor, image, time, pyb
-
+from pyb import Pin ,UART,LED
 start = Pin('P0',Pin.IN,Pin.PULL_UP)
 lighton = Pin('P1',Pin.IN,Pin.PULL_UP)
 status = Pin('P7',Pin.OUT_PP,Pin.PULL_NONE)
-from pyb import Pin ,UART,LED
+deta = Pin('P4',Pin.OUT_PP,Pin.PULL_NONE)
+intr = Pin('P5',Pin.OUT_PP,Pin.PULL_NONE)
+
 
 sensor.reset()
 sensor.set_pixformat(sensor.RGB565)
@@ -17,7 +19,17 @@ clock = time.clock()
 state = False;
 
 while(True):
-    if(start.value())
+    if start.value() == True:
+        intr.on()
+        time.sleep_ms(100)
+        intr.off()
+
+    if lighton.value() == True:
+        state = not state
+        deta.value(state);
+        status.value(state)
+        time.sleep_ms(100)
+
     clock.tick()
     img = sensor.snapshot()
-    print(clock.fps())
+    print(state)
