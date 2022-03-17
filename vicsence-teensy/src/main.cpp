d310ffa06d8d05d6720e29a592f0f060ab662b7a
K_HazK <hajime.nishimura0502@gmail.com>
2022-03-17 20:06:12 +0900

    kossan記述分追加
    サブteensyのプログラム

---
+++ vicsence-teensy/src/main.cpp
@@ -0,0 +1,41 @@
+#include <Arduino.h>
+#include <Wire.h>
+#include "read_camera.h"
+#include "read_temperature.h"
+#include "detect_victim.h"
+
+// read_camera camR(&Serial1);
+// read_temperature d6tR(&Wire);
+
+read_camera camL(&Serial2);
+read_temperature d6tL(&Wire1);
+// detect_victim victim(&Serial1, &Serial2, &Wire, &Wire1);
+
+uint8_t interruptPin=21;
+int vicNumR, vicNumL;
+float tempR, tempL;
+
+void setup() {
+  Serial.begin(115200);
+  Wire.begin();
+  Wire1.begin();
+  pinMode(interruptPin, OUTPUT);
+}
+
+void loop() {
+  // vicNumR=camR.victim_num();
+  // tempR=d6tR.temp();
+  // Serial.print(vicNumR);  Serial.print("\t");
+  // Serial.print(tempR);  Serial.print("\t");
+  vicNumL=camL.victim_num();
+  tempL=d6tL.temp();
+  Serial.print(vicNumL);  Serial.print("\t");
+  Serial.print(tempL);  Serial.print("\t");
+  Serial.println("");
+  if(tempL>30.0){
+    digitalWrite(interruptPin, HIGH);
+  }else{
+    digitalWrite(interruptPin, LOW);
+  }
+  delay(50);
+}
