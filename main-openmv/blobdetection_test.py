020a68ac12cfa1db55269a89c08efd1119243ea3
kossansato <kossan.sato@gmail.com>
2022-04-21 00:17:35 +0900

    modify the letter detection
    variable circle radius
    change the distinction logic

--- main-openmv/blobdetection_test.py
+++ main-openmv/blobdetection_test.py
@@ -16,12 +16,12 @@ red_led   = LED(1)
 green_led = LED(2)
 blue_led  = LED(3)
 
-tim = Timer(4, freq=2000)
+tim = Timer(4, freq=20000)
 
 #led.value(False)
 while(start.value() == False):
     if lighton.value() == True:
-        ch1 = tim.channel(1, Timer.PWM, pin=Pin("P7"), pulse_width_percent=10)
+        ch1 = tim.channel(1, Timer.PWM, pin=Pin("P7"), pulse_width_percent=2)
         #led.value(not led.value())
         time.sleep_ms(500)
 
@@ -42,6 +42,7 @@ thr = [(0, 26, -128, 127, -128, 127),(31, 41, -40, -19, 7, 26),(75, 84, -1, 15, 
 min_degree = 0
 max_degree = 179
 result = 'N'
+blob_diagonal_sq = 6000 # <- proper size for existing algorithm
 status.on()
 
 clock = time.clock()
@@ -54,15 +55,16 @@ while(True):
     blobcode = 0
     for blob in img.find_blobs(thr, pixels_threshold=500, area_threshold=500,merge = True):
         linecoor = [0,0]
-        for i in range(0,len(linecoor)):
+        for i in range(0,len(linecoor)):    #drawing tricolore-like lines
             linecoor[i] = blob.x() + (blob.w()*(i+1))/3
             divhist.append(img.get_histogram(roi = [blob.x(),blob.y(),int(linecoor[i]),blob.h()]))
-            img.draw_line(int(linecoor[i]),blob.y(),int(linecoor[i]),blob.y() + blob.h())
+            #img.draw_line(int(linecoor[i]),blob.y(),int(linecoor[i]),blob.y() + blob.h())
             divstati.append(divhist[i].get_statistics())
             #print(divstati[i].l_mean())
 
         #print(blob.code())
         blobcode = blob.code()
+        blob_diagonal_sq = blob.w()*blob.w()+blob.h()*blob.h()
         if blobcode==1:#black
             linecount = 0
             for l in img.find_lines(roi = blob.rect() ,threshold = 1200, theta_margin = 50, rho_margin = 20):#直線検知しきい値 あまりいじる必要なし
@@ -70,17 +72,18 @@ while(True):
                     img.draw_line(l.line(), color = (255, 0, 0))
                     linecount += 1
             circlecount = 0
-            for c in img.find_circles(roi = blob.rect(), threshold = 1800, x_margin = 10, y_margin = 20, r_margin = 100,
-            r_min = 10, r_max = 20, r_step = 2):
+            circle_r=int(blob_diagonal_sq*0.00167) #dividing by 600
+            for c in img.find_circles(roi = blob.rect(), threshold = 2000, x_margin = 10, y_margin = 20, r_margin = 100,
+            r_min = circle_r, r_max = circle_r + 10, r_step = 2):
                 img.draw_circle(c.x(), c.y(), c.r(), color = (255, 0, 0))
                 circlecount += 1
             if linecount == 0 and circlecount == 0:
                 result = 'U'
             elif circlecount >= 2 and linecount <= 1:
                 result = 'S'
-            elif linecount == 2 and circlecount >= 1:
+            elif linecount >= 2 and linecount <=3 and circlecount >= 1:
                 result = 'U'
-            elif linecount == 3:
+            elif linecount >= 3:
                 result = 'H'
             else:
                 result = 'N'
@@ -97,7 +100,8 @@ while(True):
         img.draw_rectangle(blob.rect())
         #for k in range(2):
 
-
-    print(clock.fps())
+    print(result)
+    print(blob_diagonal_sq)
+    #print(clock.fps())
     uart.write(result)
     result = 'N'
